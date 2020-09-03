bd3da83ebbd40006bb72522edbf897db84f30dde
cris12gm <cris12gm@gmail.com>
2020-09-03 12:52:42 +0200

    mongoTools

---
+++ mongoTools/classes/methylation.py
@@ -0,0 +1,13 @@
+import ipaddress
+import re
+
+from base.item import Item
+
+class Methylation(Item):
+    collection_name = 'methylation'
+    collection_schema = {
+        'methylation_CG': 1
+    }
+
+    def __init__(self):
+        super(Methylation, self).__init__()
